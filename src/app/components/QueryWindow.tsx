dea06262c2c05774a4899a8f60cbddacb4e2d695
drkchiloll <samwomack_@me.com>
2017-04-18 19:32:18 -0500

    feature update

--- src/app/components/QueryWindow.tsx
+++ src/app/components/QueryWindow.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import * as brace from 'brace';
 import AceEditor from 'react-ace';
+
+import * as $ from 'jquery'
  
 import 'brace/mode/mysql';
 import 'brace/theme/monokai';
@@ -9,7 +11,8 @@ import 'brace/keybinding/vim';
 import {
   Paper, TextField, Divider, Drawer,
   Subheader, List, ListItem, makeSelectable,
-  BottomNavigation, BottomNavigationItem
+  BottomNavigation, BottomNavigationItem,
+  Toggle
 } from 'material-ui';
 
 let SelectableList = makeSelectable(List);
@@ -18,9 +21,12 @@ export class QueryWindow extends React.Component<any,any> {
   constructor() {
     super();
     this.state = {
+      aceFocus: false,
+      vimMode: false,
+      editor: null,
       selectedStatement: null,
-      drawerWidth: 324,
-      editorWidth: 720,
+      drawerWidth: 310,
+      editorWidth: 700,
       selectedQuery: 0,
       queries: [{
         name: 'q1',
@@ -34,8 +40,10 @@ export class QueryWindow extends React.Component<any,any> {
     this._newQuery = this._newQuery.bind(this);
     this._execQuery = this._execQuery.bind(this);
     this._saveQuery = this._saveQuery.bind(this);
+    this._setEditorMode = this._setEditorMode.bind(this);
   }
   componentWillMount() {
+    if(this.props.view==='mainView') this.setState({ aceFocus: true });
     let { selectedQuery, selectedStatement, queries } = this.state;
     if(!selectedStatement) selectedStatement = queries[selectedQuery].query;
     this.setState({ selectedStatement });
@@ -47,27 +55,62 @@ export class QueryWindow extends React.Component<any,any> {
       this.setState({ editorWidth });
     };
   }
+  componentWillReceiveProps(nextProps) {
+    let aceFocus = nextProps.view==='mainView' ? true: false;
+    this.setState({ aceFocus });
+  }
   _queryChange(e, value) {
     let selectedStatement = this.state.queries[value].query;
     this.setState({
       selectedQuery: value,
       selectedStatement
     });
+    setTimeout(() => {
+      let editor = this.state.editor,
+          row = editor.session.getLength() - 1,
+          column = editor.session.getLine(row).length;
+      editor.focus();
+      editor.gotoLine(row + 1, column);
+    },0);
   }
   _newQuery() {
-
+    let { selectedStatement, selectedQuery, queries } = this.state,
+        current = selectedStatement,
+        old = queries[selectedQuery].query;
+    if(current !== old) {
+      // We Need to Prompt the User if they Want to Save the Previous
+    }
+    selectedStatement = '';
+    selectedQuery = queries.length + 1;
+    this.setState({ selectedStatement, selectedQuery });
+    $(this.state.editor).focus();
   }
   _execQuery() {
 
   }
+  focusEditor() {
+    return true;
+  }
   _saveQuery() {
-
+    console.log('save');
+    let { selectedStatement, selectedQuery, queries } = this.state,
+        current = selectedStatement,
+        old = queries[selectedQuery].query;
+    console.log(current);
+    console.log(old);
+    if(current !== old) {
+      console.log('statement changed');
+    }
+  }
+  _setEditorMode(e, checked) {
+    const ace = require('brace');
+    if(checked) this.state.editor.setKeyboardHandler('ace/keyboard/vim');
+    else this.state.editor.setKeyboardHandler('');
+    this.setState({ vimMode: checked });
+    this.state.editor.focus();
   }
   render() {
-    let aceFocus = this.props.view==='mainView' ? true : false;
-    if(this.props.save) {
-      console.log(this.state.selectedStatement);
-    }
+    let aceFocus = this.state.aceFocus;
     return (
       <div>
         <Drawer open={true} width={this.state.drawerWidth}
@@ -92,59 +135,87 @@ export class QueryWindow extends React.Component<any,any> {
             </SelectableList>
           </div>
         </Drawer>
-        <div style={{marginLeft:310}}>
+        <div style={{position:'fixed', left: 310}}>
+          <BottomNavigation
+            style={{
+              backgroundColor: '#d7dddd',
+              height: 75
+            }}>
+            <BottomNavigationItem
+              icon={
+                <span className="fa-stack fa-lg">
+                  <i className="fa fa-database fa-stack-2x fa-inverse"></i>
+                  <i className="fa fa-play-circle fa-stack-1x"
+                    style={{margin: '10px 0 0 15px'}} />
+                </span>
+              }
+              label='Execute SQL'
+              onTouchTap={this._execQuery}/>
+            <BottomNavigationItem
+              icon={
+                <span className="fa-stack fa-lg">
+                  <i className="fa fa-square fa-stack-2x"></i>
+                  <i className="fa fa-terminal fa-stack-1x fa-inverse"></i>
+                </span>
+              }
+              label='New Query'
+              onTouchTap={this._newQuery}/>
+            <BottomNavigationItem
+              className='save-query'
+              icon={
+                <span className='fa-stack fa-lg'>
+                  <i className='fa fa-hdd-o fa-stack-2x'/>
+                </span>
+              }
+              label='Save'
+              onClick={this._saveQuery}/>
+          </BottomNavigation>
           <AceEditor
             mode='mysql'
             theme='monokai'
+            onLoad={(editor) => {
+              const ace = require('brace'),
+                    Vim = ace.acequire('ace/keyboard/vim').CodeMirror.Vim;
+              Vim.defineEx('write', 'w', (cm, input) => {
+                this._saveQuery();
+              });
+              this.setState({ editor });
+            }}
             onChange={(sql) => {
               this.setState({ selectedStatement: sql });
             }}
+            className='editor'
+            focus={aceFocus}
+            commands={[
+              {
+                name:'save',
+                bindKey: {
+                  win: 'Ctrl-S', mac: 'Command-S'
+                },
+                exec: function() {
+                  $('.save-query').get(0).click();
+                }
+              }
+            ]}
             name='editor'
             height='200px'
-            width={this.state.editorWidth+'px'}
+            width={`${this.state.editorWidth}px`}
             tabSize={2}
             highlightActiveLine={false}
-            focus={aceFocus}
             value={this.state.selectedStatement}
             enableBasicAutocompletion={true}
             enableLiveAutocompletion={true}
-            keyboardHandler='vim'
             editorProps={{$blockScrolling: 1}}/>
-          <Paper zDepth={2} >
-            <BottomNavigation
-              style={{
-                backgroundColor: '#d7dddd',
-                height: 75
-              }}>
-              <BottomNavigationItem
-                icon={
-                  <span className="fa-stack fa-lg">
-                    <i className="fa fa-database fa-stack-2x fa-inverse"></i>
-                    <i className="fa fa-play-circle fa-stack-1x"
-                      style={{margin: '10px 0 0 15px'}} />
-                  </span>
-                }
-                label='Execute SQL'
-                onTouchTap={this._execQuery}/>
-              <BottomNavigationItem
-                icon={
-                  <span className="fa-stack fa-lg">
-                    <i className="fa fa-square fa-stack-2x"></i>
-                    <i className="fa fa-terminal fa-stack-1x fa-inverse"></i>
-                  </span>
-                }
-                label='New Query'
-                onTouchTap={this._newQuery}/>
-              <BottomNavigationItem
-                icon={
-                  <span className='fa-stack fa-lg'>
-                    <i className='fa fa-save fa-stack-2x'/>
-                  </span>
-                }
-                label='Save'
-                onTouchTap={this._saveQuery}/>
-            </BottomNavigation>
-          </Paper>
+          <div style={{float:'right'}}>
+            <Toggle
+              label='Enable VIM Mode'
+              toggled={this.state.vimMode}
+              trackStyle={{backgroundColor: 'red'}}
+              thumbStyle={{backgroundColor:'#ffcccc'}}
+              thumbSwitchedStyle={{backgroundColor: '#72d86e'}} //Thumb Checked/On
+              trackSwitchedStyle={{backgroundColor: 'green'}} // Track Checked/On
+              onToggle={this._setEditorMode} />
+          </div>
         </div>
       </div>
     );
