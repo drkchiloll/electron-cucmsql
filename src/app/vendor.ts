8c8f10603afe3aa4425bd390675722c90f8ab709
drkchiloll <samwomack_@me.com>
2017-04-25 21:57:50 -0500

    initialize and export the editorConfig instance

--- src/app/vendor.ts
+++ src/app/vendor.ts
@@ -1,7 +1,29 @@
 // Import 3rd Parties
 // import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.min.css';
-// Import Custom API
-import './lib/api';
 // FIXED-DATA-TABLE
 import 'fixed-data-table-2/dist/fixed-data-table.min.css';
+
+// Perform Some Initialization of the API
+import { Api } from './lib/api';
+
+export declare let editorConfig;
+
+editorConfig = new Api({
+  db: 'editorDb', dbName: 'editor-config'
+});
+editorConfig.get().then((settings:any) => {
+  if(settings.length === 0) {
+    let vimMode = false,
+        fontSize = 18;
+    editorConfig.vimMode = vimMode;
+    editorConfig.fontSize = fontSize;
+    editorConfig.add({ vimMode, fontSize });
+  } else {
+    let [ { vimMode, fontSize, _id } ] = settings;
+    editorConfig.recordId = _id;
+    editorConfig.vimMode = vimMode;
+    editorConfig.fontSize = fontSize;
+  }
+});
+
