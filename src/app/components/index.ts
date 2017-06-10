77d91f94ed84103ee518cdf8cd4e1b3d80807de7
drkchiloll <samwomack_@me.com>
2017-06-10 09:07:09 -0500

    add all dependency exports for easier more tidy imports

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,3 +1,43 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as brace from 'brace';
+import * as Promise from 'bluebird';
+import AceEditor from 'react-ace';
+import { Api } from '../lib/api';
+import { CucmSql } from '../lib/cucm-sql';
+import { editorConfig } from '../vendor';
+import * as $ from 'jquery'
+ 
+import 'brace/mode/mysql';
+import 'brace/theme/monokai';
+import 'brace/keybinding/vim';
+
+import { Table, Column, Cell } from 'fixed-data-table-2';
+
+import {
+  Paper, TextField, Divider, Drawer,
+  Subheader, List, ListItem, makeSelectable,
+  BottomNavigation, BottomNavigationItem,
+  Toggle, Dialog, FlatButton, Chip, Avatar
+} from 'material-ui';
+
+import {
+  indigo900, blue300, red300
+} from 'material-ui/styles/colors';
+
+import SvgIconErrorOutline from 'material-ui/svg-icons/alert/error-outline';
+
+export {
+  React, ReactDOM, brace,
+  Promise, AceEditor, Api, CucmSql,
+  editorConfig, $, Table, Column, Cell,
+  Paper, TextField, Divider, Drawer,
+  Subheader, List, ListItem, makeSelectable,
+  BottomNavigation, BottomNavigationItem,
+  Toggle, Dialog, FlatButton, Chip, Avatar,
+  indigo900, blue300, red300, SvgIconErrorOutline
+};
+
 // Component Exports
 export { App } from './App';
 export { Accounts } from './Accounts';
