62eb8dc1c67bf24952cdb168f0de2e88da094dff
drkchiloll <samwomack_@me.com>
2018-02-20 16:17:07 -0600

    clean up out of date code - material-ui

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,18 +3,8 @@ import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import * as $ from 'jquery';
 import { App } from './components';
-
-// MATERIAL-UI DEPENDENCIES
-import * as injectTapEventPlugin from 'react-tap-event-plugin';
-// Needed for onTouchTap
-// Check this repo:
-// https://github.com/zilverline/react-tap-event-plugin
-injectTapEventPlugin();
-
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import {
-  MuiThemeProvider
-} from "material-ui/styles";
+import { MuiThemeProvider } from "material-ui/styles";
 import { darkBlack, fullWhite } from 'material-ui/styles/colors';
 
 const lightMuiTheme = getMuiTheme({
