c07867f1bf401fe6740c567e4734c219efe7c376
drkchiloll <sam.womack@entropicos.com>
2019-11-17 13:26:41 -0600

    export component properly with TypeScript

--- src/app/components/ExportCsvButton.tsx
+++ src/app/components/ExportCsvButton.tsx
@@ -1,4 +1,5 @@
-import { React, CsvCreator, FlatButton, FontIcon } from './index';
+import { React, FlatButton, FontIcon } from './index';
+import * as CsvCreator from 'react-csv-creator';
 
 const styles: any = {
   main: { width: 50 },
@@ -8,25 +9,27 @@ const styles: any = {
   }
 };
 
-export function ExportCsvButton(props: any) {
-  return (
-    <div style={styles.main}>
-      <CsvCreator
-        filename='export'
-        headers={props.headers}
-        rows={props.rows}
-      >
-        <FlatButton 
-          label='EXPORT'
-          labelPosition='before'
-          primary={true}
-          icon={
-            <FontIcon
-              color='blue'
-              className='fa fa-external-link'
-              style={styles.icon} />
-          } />
-      </CsvCreator>
-    </div>
-  );
+export class ExportCsvButton extends React.Component<any, any> {
+  render() {
+    return (
+      <div style={styles.main}>
+        <CsvCreator
+          filename='export'
+          headers={this.props.headers}
+          rows={this.props.rows}
+        >
+          <FlatButton 
+            label='EXPORT'
+            labelPosition='before'
+            primary={true}
+            icon={
+              <FontIcon
+                color='blue'
+                className='fa fa-external-link'
+                style={styles.icon} />
+            } />
+        </CsvCreator>
+      </div>
+    );
+  }
 }
