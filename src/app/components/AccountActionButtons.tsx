e998bd41118a08cfcfa301d82b0148f00780cdb5
drkchiloll <sam.womack@entropicos.com>
2019-11-23 15:31:50 -0600

    renamed from previous component

---
+++ src/app/components/AccountActionButtons.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import {
+	BottomNavigation,
+	BottomNavigationItem,
+	FontIcon
+} from 'material-ui';
+
+export const AccountActionButtons = (props: any) => (
+	<BottomNavigation
+		style={{ position:'fixed', bottom: 0 }}
+	>
+		<BottomNavigationItem
+			label="Account"
+			icon={<FontIcon className='fa fa-user-plus'/>}
+			onClick={props.handleAccountAddClick}
+		/>
+		<BottomNavigationItem
+			label="Remove"
+			icon={<FontIcon color='red' className='fa fa-trash'/>}
+			onClick={props.deleteAccount}
+		/>
+	</BottomNavigation>
+)
